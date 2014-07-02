1adb005baf5797fb5c888ae778a52998bf9c2f88
Krzysztof Ciomek <k.ciomek@gmail.com>
2014-07-02 20:48:40 +0200

    Improved RORUTADIS-GroupAssignments module.

--- RORUTADIS-GroupAssignments-PUT/RORUTADIS-GroupAssignments.R
+++ RORUTADIS-GroupAssignments-PUT/RORUTADIS-GroupAssignments.R
@@ -11,19 +11,18 @@ outDirectory <- normalizePath(commandArgs()[6])
 errorMessage <- NULL
 
 inputFiles <- c("alternatives.xml",
-                "categories.xml",
-                "necessary.xml")
-isMandatory <- c(T, T, T)
+                "categories.xml")
+isMandatory <- c(T, T)
 
 trees <- list()
 setwd(inDirectory)
 
-allFiles <- list.files(".")
-for (file in allFiles) {
-  if (substring(file, 1, 11) == "assignments") {
-    inputFiles <- c(inputFiles, file)
-    isMandatory <- c(isMandatory, T)
-  }
+necessaryAssignmentsFiles <- list.files(".", pattern="^necessaryAssignmentsDM\\d+\\.xml$")
+possibleAssignmentsFiles <- list.files(".", pattern="^possibleAssignmentsDM\\d+\\.xml$")
+
+for (file in c(necessaryAssignmentsFiles, possibleAssignmentsFiles)) {
+  inputFiles <- c(inputFiles, file)
+  isMandatory <- c(isMandatory, T)
 }
 
 for (i in seq_len(length(inputFiles))) {
@@ -37,9 +36,9 @@ for (i in seq_len(length(inputFiles))) {
     if (inherits(tmpErr, 'try-error')) {
       trees[[i]] <- paste("Error reading file ", inputFiles[i],": ", gsub("\n$", "", tmpErr[1]), sep = "")
     } else if (checkXSD(tree) == 0) {
-        trees[[i]] <- paste(inputFiles[i], " is not XMCDA valid.", sep="")
+      trees[[i]] <- paste(inputFiles[i], " is not XMCDA valid.", sep="")
     } else {
-        trees[[i]] <- tree
+      trees[[i]] <- tree
     }
   } else {
     if (isMandatory[i]) {
@@ -53,13 +52,31 @@ for (i in seq_len(length(inputFiles))) {
 names(trees) <- gsub("\\.xml$", "", inputFiles)
 
 fileErrors <- which(sapply(trees, typeof) == "character")
-    
+
 if (length(fileErrors) == 0) {
   # There was not file error.
   dataError <- NULL
   
   if (length(trees) <= 3) {
     dataError <- "No assignments file found."
+  }
+  
+  for (file in necessaryAssignmentsFiles) {
+    dmId <- gsub("(^necessaryAssignmentsDM)|(\\.xml$)", "", file)
+    
+    if (!(paste("possibleAssignmentsDM", dmId, ".xml", sep = "") %in% possibleAssignmentsFiles)) {
+      dataError <- paste("Possible assignments for DM", dmId, " not found.", sep = "")
+      break
+    }
+  }
+  
+  for (file in possibleAssignmentsFiles) {
+    dmId <- gsub("(^possibleAssignmentsDM)|(\\.xml$)", "", file)
+    
+    if (!(paste("necessaryAssignmentsDM", dmId, ".xml", sep = "") %in% necessaryAssignmentsFiles)) {
+      dataError <- paste("Necessary assignments for DM", dmId, " not found.", sep = "")
+      break
+    }
   }
   
   ############# mandatory
@@ -92,37 +109,46 @@ if (length(fileErrors) == 0) {
     else dataError <- data$status
   }
   
-  ############# parameters
-  
-  if (is.null(dataError)) {
-    data <- getParameters(trees$necessary, "necessary")
-    if (data$status == "OK") necessary <- data[[1]]
-    else dataError <- data$status
-  }
-  
   ############# assignments
   
   inputAffectations <- list()
   
-  for (i in 4:length(trees)) {
-    data <- getAlternativesAffectations(trees[[i]], alternativesIDs, categoriesIDs)
-    if (data$status == "OK") {
-      inputAffectations[[length(inputAffectations) + 1]] <- data[[1]]
+  if (is.null(dataError)) {
+    for (i in 4:length(trees)) {
+      data <- getAlternativesAffectations(trees[[i]], alternativesIDs, categoriesIDs)
+      if (data$status == "OK") {
+        inputAffectations[[length(inputAffectations) + 1]] <- data[[1]]
+      }
+      else {
+        dataError <- data$status
+        break
+      }
     }
-    else {
-      dataError <- data$status
-      break
-    }
+  
+    names(inputAffectations) <- names(trees)[-c(1, 2, 3)]
   }
   
   if (is.null(dataError)) {
     tmpErr <- try({
-      alternativesAffectations <- mergeAssignments(inputAffectations, necessary)
+      necessaryAssignmentDataIds <-
+        which(unlist(sapply(names(inputAffectations), function(name) { substr(name, 0, 9) == "necessary"} )))
+      possibleAssignmentDataIds <-
+        which(unlist(sapply(names(inputAffectations), function(name) { substr(name, 0, 8) == "possible"} )))
+      
+      necessaryNecessaryAlternativesAffectations <-
+        mergeAssignments(inputAffectations[necessaryAssignmentDataIds], TRUE)
+      necessaryPossibleAlternativesAffectations <-
+        mergeAssignments(inputAffectations[necessaryAssignmentDataIds], FALSE)
+      possibleNecessaryAlternativesAffectations <- 
+        mergeAssignments(inputAffectations[possibleAssignmentDataIds], TRUE)
+      possiblePossibleAlternativesAffectations <- 
+        mergeAssignments(inputAffectations[possibleAssignmentDataIds], FALSE)
     })
     
     if (inherits(tmpErr, 'try-error')) {
       # Execution error.
       errorMessage <- paste("Execution error: ", gsub("\n$", "", gsub("^.*: ", "", tmpErr[1])), sep = "")
+      print (errorMessage)
     }
     else {
       # Success.
@@ -134,8 +160,35 @@ if (length(fileErrors) == 0) {
                  suppressNamespaceWarning = TRUE, 
                  namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2009/XMCDA-2.0.0"), 
                  parent = tree)
-      putAlternativesAffectations(tree, alternativesAffectations, alternativesIDs, categoriesIDs, TRUE)
-      saveXML(tree, file = "assignments.xml")
+      putAlternativesAffectations(tree, necessaryNecessaryAlternativesAffectations, alternativesIDs, categoriesIDs, TRUE)
+      saveXML(tree, file = "necessaryNecessaryAssignments.xml")
+      
+      tree <- newXMLDoc()
+      newXMLNode("xmcda:XMCDA", 
+                 attrs = c("xsi:schemaLocation" = "http://www.decision-deck.org/2009/XMCDA-2.0.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.0.0.xsd"),
+                 suppressNamespaceWarning = TRUE, 
+                 namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2009/XMCDA-2.0.0"), 
+                 parent = tree)
+      putAlternativesAffectations(tree, necessaryPossibleAlternativesAffectations, alternativesIDs, categoriesIDs, TRUE)
+      saveXML(tree, file = "necessaryPossibleAssignments.xml")
+      
+      tree <- newXMLDoc()
+      newXMLNode("xmcda:XMCDA", 
+                 attrs = c("xsi:schemaLocation" = "http://www.decision-deck.org/2009/XMCDA-2.0.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.0.0.xsd"),
+                 suppressNamespaceWarning = TRUE, 
+                 namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2009/XMCDA-2.0.0"), 
+                 parent = tree)
+      putAlternativesAffectations(tree, possibleNecessaryAlternativesAffectations, alternativesIDs, categoriesIDs, TRUE)
+      saveXML(tree, file = "possibleNecessaryAssignments.xml")
+      
+      tree <- newXMLDoc()
+      newXMLNode("xmcda:XMCDA", 
+                 attrs = c("xsi:schemaLocation" = "http://www.decision-deck.org/2009/XMCDA-2.0.0 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.0.0.xsd"),
+                 suppressNamespaceWarning = TRUE, 
+                 namespace = c("xsi" = "http://www.w3.org/2001/XMLSchema-instance", "xmcda" = "http://www.decision-deck.org/2009/XMCDA-2.0.0"), 
+                 parent = tree)
+      putAlternativesAffectations(tree, possiblePossibleAlternativesAffectations, alternativesIDs, categoriesIDs, TRUE)
+      saveXML(tree, file = "possiblePossibleAssignments.xml")
     }
   }
   else {
